8baa47a48321ab74a99bb037e58e49f7767f0e19
Luis de Olazabal <luchoschettiniazur@outlook.com>
2023-06-02 21:45:39 +0200

    Primera subida del codigo

---
+++ Shooping/Shooping/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shooping.Data.Entities;
+
+namespace Shooping.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> option): base(option)
+    {
+
+    }
+
+    public DbSet<Country> Countries { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+    }
+
+}
